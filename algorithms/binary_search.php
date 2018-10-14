2cf7bdfcec4c7f23944faf6e8ef3ceb2da743357
f1r3starter <andrey.filenochko@gmail.com>
2018-10-14 19:39:33 +0300

    added binary search example

---
+++ algorithms/binary_search.php
@@ -0,0 +1,22 @@
+<?php
+
+$searching = 2;
+
+$array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 17, 19, 40, 42];
+
+$length = count($array);
+$iterations = $position = 0;
+
+while ($length - $position >= 1) {
+    ++$iterations;
+    $index = (int)(($position + $length) / 2);
+    if ($array[$index] === $searching) {
+        die(sprintf('Searching index is %d in %d iterations', $index, $iterations));
+    } elseif ($array[$index] < $searching) {
+        $position = $index;
+    } else {
+        $length = $index;
+    }
+}
+
+die('Value not found');
