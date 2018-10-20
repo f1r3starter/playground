8b89929f1026a068e4cd83c22ea43c07429905b8
f1r3starter <andrey.filenochko@gmail.com>
2018-10-20 19:32:10 -0400

    added magic methods

---
+++ php/magic_methods.php
@@ -0,0 +1,67 @@
+<?php
+
+class Magician {
+    public function __construct()
+    {
+    }
+
+    public function __destruct()
+    {
+        // TODO: Implement __destruct() method.
+    }
+
+    public function __call($name, $arguments)
+    {
+        // TODO: Implement __call() method.
+    }
+
+    public function __clone()
+    {
+        // TODO: Implement __clone() method.
+    }
+
+    public function __get($name)
+    {
+        // TODO: Implement __get() method.
+    }
+
+    public function __set($name, $value)
+    {
+        // TODO: Implement __set() method.
+    }
+
+    public function __invoke()
+    {
+        // TODO: Implement __invoke() method.
+    }
+
+    public function __isset($name)
+    {
+        // TODO: Implement __isset() method.
+    }
+
+    public function __unset($name)
+    {
+        // TODO: Implement __unset() method.
+    }
+
+    public function __sleep()
+    {
+        // TODO: Implement __sleep() method.
+    }
+
+    public function __wakeup()
+    {
+        // TODO: Implement __wakeup() method.
+    }
+
+    public function __toString()
+    {
+        // TODO: Implement __toString() method.
+    }
+
+    public function __debugInfo()
+    {
+        // TODO: Implement __debugInfo() method.
+    }
+}
