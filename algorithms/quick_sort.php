0183733ab24cec7874af6ed8e72fe8cd480e1b30
f1r3starter <andrey.filenochko@gmail.com>
2018-12-09 20:11:01 +0200

    quick sort

--- algorithms/quick_sort.php
+++ algorithms/quick_sort.php
@@ -1,2 +1,22 @@
 <?php
 
+function quick_sort($arr)
+{
+    if (count($arr) < 2) {
+        return $arr;
+    }
+    $pivotNum = $arr[floor(count($arr) / 2)];
+    $result = $higher = $lower = [];
+    foreach ($arr as $num)
+    {
+        if ($num > $pivotNum) {
+            $higher[] = $num;
+        } elseif ($num < $pivotNum) {
+            $lower[] = $num;
+        } else {
+            $result[] = $num;
+        }
+    }
+
+    return array_merge(quick_sort($lower), $result, quick_sort($higher));
+}
