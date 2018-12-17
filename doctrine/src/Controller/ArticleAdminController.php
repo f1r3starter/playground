13b05109ada1a438c17c02b541ab3fbfbd450dc7
f1r3starter <andrey.filenochko@gmail.com>
2018-12-17 22:28:48 +0200

    some controller logic

---
+++ doctrine/src/Controller/ArticleAdminController.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andreyfilenko
+ * Date: 2018-12-17
+ * Time: 21:41
+ */
+
+namespace App\Controller;
+
+
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleAdminController extends AbstractController
+{
+    /**
+     * @Route("/admin/article/new")
+     */
+    public function new(EntityManagerInterface $entityManager)
+    {
+        $article = new Article();
+        $article->setTitle('Blah blah')
+            ->setSlug('blah-blah' . rand(100,200))
+            ->setContent('blah blah')
+            ->setPublishedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
+        $entityManager->persist($article);
+        $entityManager->flush();
+
+        return new Response(sprintf('It was saved, id %d, slug %s', $article->getId(), $article->getSlug()));
+    }
+}
