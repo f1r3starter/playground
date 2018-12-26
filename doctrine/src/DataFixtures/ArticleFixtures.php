c296fa7a411771f7f66ff84e52296efc99164c3b
f1r3starter <andrey.filenochko@gmail.com>
2018-12-26 23:38:38 +0200

    sluggable

--- doctrine/src/DataFixtures/ArticleFixtures.php
+++ doctrine/src/DataFixtures/ArticleFixtures.php
@@ -11,9 +11,7 @@ class ArticleFixtures extends BaseFixtures
     {
         $this->createMany(Article::class, 10, function (Article $article, int $count) {
             $article->setTitle($this->faker->sentence(3))
-                ->setSlug($this->faker->slug)
                 ->setContent($this->faker->text(300))
-
                 ->setAuthor($this->faker->name())
                 ->setLikes($this->faker->numberBetween(5, 100))
                 ->setImage($this->faker->image('public/images', 640, 480, null, false));
