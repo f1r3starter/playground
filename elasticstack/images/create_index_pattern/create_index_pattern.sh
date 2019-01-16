3964e52145a723ede789b300b135c0a916701511
f1r3starter <andrey.filenochko@gmail.com>
2019-01-16 19:56:43 +0200

    docker compose create index pattern

--- elasticstack/images/create_index_pattern/create_index_pattern.sh
+++ elasticstack/images/create_index_pattern/create_index_pattern.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-curl -f -XPOST -H "Content-Type: application/json" -H "kbn-xsrf: anything" \
-  "http://kibana:5601/api/saved_objects/index-pattern/filebeat" \
-  -d"{\"attributes\":{\"title\":\"filebeat*\",\"timeFieldName\":\"@timestamp\"}}" \
+while :
+do
+    if health="$(curl -s http://kibana:5601/api/status)"; then
+        if [ "$health" != 'Kibana server is not ready yet' ]; then
+            curl -f -XPOST -H "Content-Type: application/json" -H "kbn-xsrf: anything" \
+              "http://kibana:5601/api/saved_objects/index-pattern/filebeat" \
+              -d"{\"attributes\":{\"title\":\"filebeat*\",\"timeFieldName\":\"@timestamp\"}}" \
 
-# Create the default index
-curl -XPOST -H "Content-Type: application/json" -H "kbn-xsrf: anything" \
-  "http://kibana:5601/api/kibana/settings/defaultIndex" \
-  -d"{\"value\":\"filebeat\"}"
+            curl -XPOST -H "Content-Type: application/json" -H "kbn-xsrf: anything" \
+              "http://kibana:5601/api/kibana/settings/defaultIndex" \
+              -d"{\"value\":\"filebeat\"}"
+            exit 0
+        fi
+    fi
+    sleep 10
+done
+
+exit 1
