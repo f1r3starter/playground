91a55035661f0c3508ab148addc7ee9ed8cdddb6
f1r3starter <andrey.filenochko@gmail.com>
2019-02-12 22:11:03 +0200

    reverse node in k groups

---
+++ codesignal/linked_list/reverseNodeInKGroups/solution.php
@@ -0,0 +1,16 @@
+<?php
+
+function reverseNodesInKGroups($l, $k) {
+    $r = $s = [];
+    do {
+        $s[] = $l->value;
+        if (count($s) === $k) {
+            $r = array_merge($r, array_reverse($s));
+            $s = [];
+        } elseif ($l->next === null) {
+            $r = array_merge($r, $s);
+        }
+        $l = $l->next;
+    } while ($l !== null);
+    return $r;
+}
