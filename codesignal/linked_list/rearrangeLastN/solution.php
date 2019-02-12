b4709538c85d17f541a71ba1fd2b167d1ccd6250
f1r3starter <andrey.filenochko@gmail.com>
2019-02-12 22:09:41 +0200

    rearrange last n

---
+++ codesignal/linked_list/rearrangeLastN/solution.php
@@ -0,0 +1,11 @@
+<?php
+
+function rearrangeLastN($l, $n) {
+    $a = [];
+    while ($l != null) {
+        $a[] = $l->value;
+        $l = $l->next;
+    }
+    $b = array_splice($a, -$n);
+    return array_merge($b, $a);
+}
