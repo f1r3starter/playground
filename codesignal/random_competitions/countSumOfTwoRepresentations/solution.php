d8b4f9eb3e5d4d67bbd66aff8dc6da1d39a052a4
f1r3starter <andrey.filenochko@gmail.com>
2019-02-13 22:09:10 +0200

    count sum of representations

---
+++ codesignal/random_competitions/countSumOfTwoRepresentations/solution.php
@@ -0,0 +1,8 @@
+<?php
+
+function countSumOfTwoRepresentations($n, $l, $r) {
+    foreach (range($l, min($n, $r)) as $k) {
+        $n - $k <= $r && $n - $k >= $l ? ++$b : '';
+    }
+    return ceil($b / 2);
+}
