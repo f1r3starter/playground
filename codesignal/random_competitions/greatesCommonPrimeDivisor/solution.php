ccbcecc395cdd836a626139654c26d4ce9bf033c
f1r3starter <andrey.filenochko@gmail.com>
2019-02-18 22:13:26 +0200

    greatest common prime divisor

---
+++ codesignal/random_competitions/greatesCommonPrimeDivisor/solution.php
@@ -0,0 +1,16 @@
+<?php
+
+function greatestCommonPrimeDivisor($a, $b) {
+    if ($a < $b) {
+        list($a,$b) = [$b, $a];
+    }
+    $rem = $a % $b;
+    if ($rem === 0) {
+        foreach (range(2,7) as $i)
+            if ($b !== $i && $b % $i == 0)
+                return greatestCommonPrimeDivisor($b, $b / $i);
+        return $b == 1 ? -1 : $b;
+    }
+
+    return greatestCommonPrimeDivisor($b, $rem);
+}
