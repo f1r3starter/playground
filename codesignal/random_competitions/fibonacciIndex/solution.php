d29861aeb5526c52acc0199f7f2bc008eb053532
f1r3starter <andrey.filenochko@gmail.com>
2019-03-06 21:48:45 +0200

    fibonacci

---
+++ codesignal/random_competitions/fibonacciIndex/solution.php
@@ -0,0 +1,11 @@
+<?php
+
+function fibonacciIndex($n) {
+    $k = 1;
+    while ($n != strlen($f)){
+        $f = $k + $m;
+        [$m,$k] = [$k,$f];
+        $i++;
+    }
+    return $n == 1 ? 0 : ++$i;
+}
