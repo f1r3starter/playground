549bfdb3772f6082ca0586ad925953b1b0057ca9
f1r3starter <andrey.filenochko@gmail.com>
2019-03-11 22:25:38 +0200

    is list palindrome

---
+++ codesignal/linked_list/isListPalindrome/solution.php
@@ -0,0 +1,21 @@
+<?php
+
+// Definition for singly-linked list:
+// class ListNode {
+//   public $value;
+//   public $next;
+//
+//   public function __construct($x) {
+//     $this->value = $x;
+//     $this->next = null;
+//   }
+// }
+//
+function isListPalindrome($l) {
+    $res = [];
+    do {
+        $res[] = $l->value;
+        $l = $l->next;
+    } while ($l !== null);
+    return $res === array_reverse($res);
+}
