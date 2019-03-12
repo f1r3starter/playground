211e559117ab02a1d03eb164ab2f39eb816be5ff
f1r3starter <andrey.filenochko@gmail.com>
2019-03-12 22:20:06 +0200

    remove k from list

---
+++ codesignal/linked_list/removeKFromList/solution.php
@@ -0,0 +1,23 @@
+<?php
+
+// Definition for singly-linked list:
+// class ListNode {
+//   public $value;
+//   public $next;
+//
+//   public function __construct($x) {
+//     $this->value = $x;
+//     $this->next = null;
+//   }
+// }
+//
+function removeKFromList($l, $k) {
+    $res = [];
+    while ($l !== null) {
+        if ($l->value !== $k) {
+            $res[] = $l->value;
+        }
+        $l = $l->next;
+    }
+    return $res;
+}
