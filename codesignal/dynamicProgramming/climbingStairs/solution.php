55102aabd5fe8c9a6dd3e15f1e3dd516c6ed6455
f1r3starter <andrey.filenochko@gmail.com>
2019-03-23 22:43:25 +0200

    climbing stairs

---
+++ codesignal/dynamicProgramming/climbingStairs/solution.php
@@ -0,0 +1,10 @@
+<?php
+
+function climbingStairs($n) {
+    $num2 = 1;
+    $num = 0;
+    for ($i = 0; $i <= $n; $i++) {
+        [$num, $num2] = [$num2, $num + $num2];
+    }
+    return $num;
+}
