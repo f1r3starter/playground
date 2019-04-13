ba2d110a67ba50e3d7f820778a4a0372a8f435c0
f1r3starter <andrey.filenochko@gmail.com>
2019-04-13 21:40:36 +0300

    first not divisible

---
+++ codesignal/random_competitions/firstNotDivisible/solution.php
@@ -0,0 +1,12 @@
+<?php
+
+function firstNotDivisible($d, $s) {
+    while (1) {
+        $t = count($d);
+        foreach ($d as $v)
+            $s % $v ? --$t : '';
+        if (!$t)
+            return $s;
+        ++$s;
+    }
+}
