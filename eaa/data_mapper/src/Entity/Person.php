c5592aaf234869592914f8e0f6b76f9c73b4d642
f1r3starter <andrey.filenochko@gmail.com>
2019-04-22 17:52:31 +0300

    data mapper

---
+++ eaa/data_mapper/src/Entity/Person.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Entity;
+
+class Person
+{
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $firstName;
+
+    /**
+     * @var string
+     */
+    protected $lastName;
+
+    /**
+     * @var string
+     */
+    protected $email;
+
+    protected function getAttributes(): array
+    {
+        return ['firstName', 'lastName', 'email'];
+    }
+
+    protected function getTableName(): string
+    {
+        return 'person';
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string $firstName
+     *
+     * @return Person
+     */
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $lastName
+     *
+     * @return Person
+     */
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     *
+     * @return Person
+     */
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
---
+++ eaa/data_mapper/src/index.php
@@ -0,0 +1,26 @@
+<?php
+
+require('../vendor/autoload.php');
+
+use App\DB\MyPDO;
+use App\Entity\Person;
+
+$myPdo = new MyPDO('mysql:host=localhost;dbname=test;charset=utf8', '', '', null);
+
+$newPerson = new Person($myPdo);
+
+$newPerson->setFirstName('Elon')
+    ->setLastName('Musk')
+    ->setEmail('elon@spacex.com');
+
+$newPerson->save();
+
+$oldPerson =  new Person($myPdo);
+
+$oldPerson->findById(1)
+    ->setFirstName('Neil')
+    ->setLastName('Armstrong');
+
+$oldPerson->save();
+
+$newPerson->delete();
