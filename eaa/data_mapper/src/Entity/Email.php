bd504a4618f2a18e8bd4b6e5146df13afc2395fe
f1r3starter <andrey.filenochko@gmail.com>
2019-04-24 22:42:20 +0300

    updated email entity

--- eaa/data_mapper/src/Entity/Email.php
+++ eaa/data_mapper/src/Entity/Email.php
@@ -16,4 +16,46 @@ class Email
      * @Column(name=EMAIL_ID,relatedClass=App\Entity\Email)
      */
     private $email;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return self
+     */
+    public function setId($id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     *
+     * @return self
+     */
+    public function setEmail($email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+
 }
