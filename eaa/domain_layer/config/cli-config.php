502f70423bb2a9e93276305831f6805ab8631337
f1r3starter <andrey.filenochko@gmail.com>
2019-05-01 20:22:08 +0300

    update mapping for entities

--- eaa/domain_layer/config/cli-config.php
+++ eaa/domain_layer/config/cli-config.php
@@ -1,8 +1,9 @@
 <?php
 
+use Doctrine\ORM\Configuration;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Driver\SimplifiedXmlDriver;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
-use Doctrine\ORM\Tools\Setup;
 
 // replace with file to your own project bootstrap
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -12,10 +13,16 @@ $conn = array(
     'driver'   => 'pdo_mysql',
     'user'     => '',
     'password' => '',
-    'dbname'   => 'hospital',
+    'dbname'   => '',
 );
 
-$config = Setup::createXMLMetadataConfiguration([__DIR__ . '/mapping'], true);
+$driver = new SimplifiedXmlDriver([__DIR__ . '/../config/mapping' => 'App\Entity']);
+$config = (new Configuration());
+$config->setAutoGenerateProxyClasses(true);
+$config->setMetadataDriverImpl($driver);
+$config->setProxyDir(__DIR__  . '/../src/Proxy');
+$config->setProxyNamespace('App\Proxy');
+
 $entityManager = EntityManager::create($conn, $config);
 
 return ConsoleRunner::createHelperSet($entityManager);
