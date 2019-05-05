8c7a2c4d6a1297da9b6b6404a9e163cf0081b19e
f1r3starter <andrey.filenochko@gmail.com>
2019-05-05 22:23:07 +0300

    http server

--- eaa/connection_pool/src/index.php
+++ eaa/connection_pool/src/index.php
@@ -1,3 +1,29 @@
 <?php
 
+use Psr\Http\Message\ServerRequestInterface;
+use React\Http\Response;
+use React\Http\Server;
+
 require '../vendor/autoload.php';
+
+
+$loop = React\EventLoop\Factory::create();
+$socket = new React\Socket\Server('127.0.0.1:8080', $loop);
+$connections = [123,321,456,745];
+
+
+$server = new Server(function (ServerRequestInterface $request) use (&$connections) {
+    $x = array_pop($connections);
+    sleep(10);
+    return new Response(
+        200,
+        array(
+            'Content-Type' => 'text/plain'
+        ),
+        (string)($x)
+    );
+});
+
+$server->listen($socket);
+
+$loop->run();
