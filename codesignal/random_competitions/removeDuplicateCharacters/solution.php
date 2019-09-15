39569b4cb7d9f5960d42be66718ca4c737b061f7
f1r3starter <andrey.filenochko@gmail.com>
2019-09-15 23:01:06 +0300

    remove duplicate

---
+++ codesignal/random_competitions/removeDuplicateCharacters/solution.php
@@ -0,0 +1,13 @@
+<?php
+
+function removeDuplicateCharacters($s)
+{
+    return implode('', array_keys(
+        array_filter(
+            count_chars($s),
+            function ($v) {
+                return $v == 1;
+            }
+        )
+    ));
+}
