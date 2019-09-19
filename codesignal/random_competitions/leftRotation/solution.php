05ce3de52da13e03be4a6ea11a1a18b8581685a1
f1r3starter <andrey.filenochko@gmail.com>
2019-09-19 22:39:51 +0300

    left rotation

---
+++ codesignal/random_competitions/leftRotation/solution.php
@@ -0,0 +1,9 @@
+<?php
+
+$handle = fopen ("php://stdin","r");
+fscanf($handle,"%d %d",$n,$k);
+$a_temp = fgets($handle);
+$a = explode(" ",$a_temp);
+array_walk($a,'intval');
+$elem = $k%$n;
+echo implode(" ", array_merge( array_slice($a, $elem),  array_slice($a, 0, $elem)));
