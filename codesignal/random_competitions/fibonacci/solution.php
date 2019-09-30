557aff96c3b24a481ded10d0c8210ee2c0ac669d
f1r3starter <andrey.filenochko@gmail.com>
2019-09-30 22:52:06 +0300

    fibonacci

---
+++ codesignal/random_competitions/fibonacci/solution.php
@@ -0,0 +1,17 @@
+<?php
+$handle = fopen ('php://stdin', 'rb');
+
+function fibonacci($n) {
+    if ($n <= 1){
+        return $n;
+    }
+    return fibonacci($n - 1) + fibonacci($n - 2);
+}
+
+$n = fgets($handle);
+
+printf('%d', fibonacci($n));
+
+fclose($handle);
+?>
+
