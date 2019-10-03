561a098c84e09f11ac8bc87ba3e0c55fe6f689ca
f1r3starter <andrey.filenochko@gmail.com>
2019-10-03 21:39:03 +0300

    quick fix

--- codesignal/random_competitions/jumpingTheClouds/solution.php
+++ codesignal/random_competitions/jumpingTheClouds/solution.php
@@ -1,9 +1,9 @@
 <?php
 
 // Complete the jumpingOnClouds function below.
-function jumpingOnClouds($c) {
+function jumpingOnClouds($c, $n) {
     $r = 0;
-    for ($i = 0; $i < count($c); $i++) {
+    for ($i = 0; $i < $n; $i++) {
         isset($c[$i+2]) && $c[$i+2] === 0 ? $i++ : '';
         $r++;
     }
@@ -20,7 +20,7 @@ fscanf($stdin, "%[^\n]", $c_temp);
 
 $c = array_map('intval', preg_split('/ /', $c_temp, -1, PREG_SPLIT_NO_EMPTY));
 
-$result = jumpingOnClouds($c);
+$result = jumpingOnClouds($c, $n);
 
 fwrite($fptr, $result . "\n");
 
