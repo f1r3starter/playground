2c82f7ad52038a1ee0764de51a79d1cd1f02e9b8
f1r3starter <andrey.filenochko@gmail.com>
2019-10-28 21:34:58 +0200

    deck

---
+++ algorithms/dequeue.php
@@ -0,0 +1,50 @@
+<?php
+
+class Deque implements Iterable
+{
+
+    // construct an empty deque
+    public function __construct()
+    {
+    }
+
+    // is the deque empty?
+    public function isEmpty(): bool
+    {
+
+    }
+
+    public function size(): int
+    {
+
+    }
+
+    public function addFirst($item): void
+    {
+
+    }
+
+    // add the item to the back
+    public function addLast($item): void
+    {
+
+    }
+
+    // remove and return the item from the front
+    public function removeFirst()
+    {
+
+    }
+
+    // remove and return the item from the back
+    public function removeLast()
+    {
+
+    }
+
+    // return an iterator over items in order from front to back
+    public function iterator()
+    {
+
+    }
+}
