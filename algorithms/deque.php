481cd47de4721c697a060d691ef85bae8a555a53
f1r3starter <andrey.filenochko@gmail.com>
2019-11-01 22:47:16 +0200

    add node

--- algorithms/deque.php
+++ algorithms/deque.php
@@ -2,26 +2,53 @@
 
 class Deque implements Iterable
 {
+    /**
+     * @var int
+     */
+    private $size = 0;
 
-    // construct an empty deque
-    public function __construct()
-    {
-    }
+    /**
+     * @var Node|null
+     */
+    private $first;
 
-    // is the deque empty?
+    /**
+     * @var Node|null
+     */
+    private $last;
+
+    /**
+     * @return bool
+     */
     public function isEmpty(): bool
     {
-
+        return $this->size === 0;
     }
 
+    /**
+     * @return int
+     */
     public function size(): int
     {
-
+        return $this->size;
     }
 
-    public function addFirst($item): void
+    /**
+     * @param object $item
+     */
+    public function addFirst(object $item): void
     {
+        $newFirst = new Node($item);
+        if ($this->isEmpty()) {
+            $this->first = $newFirst;
+            $this->last = $newFirst;
+        } else {
+            $this->first->setPrev($newFirst);
+            $newFirst->setNext($this->first);
+            $this->first = $newFirst;
+        }
 
+        ++$this->size;
     }
 
     // add the item to the back
@@ -46,5 +73,75 @@ class Deque implements Iterable
     public function iterator()
     {
 
+    }
+}
+
+class Node {
+    /**
+     * @var object
+     */
+    private $value;
+
+    /**
+     * @var Node|null
+     */
+    private $prev;
+
+    /**
+     * @var Node|null
+     */
+    private $next;
+
+    public function __construct(object $value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return object
+     */
+    public function getValue(): object
+    {
+        return $this->value;
+    }
+
+    /**
+     * @return Node|null
+     */
+    public function getPrev(): ?Node
+    {
+        return $this->prev;
+    }
+
+    /**
+     * @param Node|null
+     *
+     * @return Node
+     */
+    public function setPrev(?Node $prev): self
+    {
+        $this->prev = $prev;
+
+        return $this;
+    }
+
+    /**
+     * @return Node|null
+     */
+    public function getNext(): ?Node
+    {
+        return $this->next;
+    }
+
+    /**
+     * @param Node|null $next
+     *
+     * @return Node
+     */
+    public function setNext(?Node $next): self
+    {
+        $this->next = $next;
+
+        return $this;
     }
 }
