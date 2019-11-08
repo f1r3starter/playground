e49ceb29330db85258defb883e05924dc2653f68
f1r3starter <andrey.filenochko@gmail.com>
2019-11-08 22:56:50 +0200

    codeception play

--- codeception/index.php
+++ codeception/index.php
@@ -3,5 +3,5 @@
 require('vendor/autoload.php');
 
 $stub = \Codeception\Stub::make(\App\DumbClass::class, [
-    'add' => \Codeception\Stub\Expected::
+    'add' => \Codeception\Stub\Expected::once()->getMatcher()->invoked()
 ]);
