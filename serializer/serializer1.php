deee3a93282d3aac3304f388a0db85d52c6750ee
f1r3starter <andrey.filenochko@gmail.com>
2019-11-22 23:12:06 +0200

    json serializable

---
+++ serializer/serializer1.php
@@ -0,0 +1,33 @@
+<?php
+
+class Test implements JsonSerializable {
+    private $a;
+    private $b;
+    private $c;
+
+    public function __construct($a, $b, $c)
+    {
+        $this->a = $a;
+        $this->b = $b;
+        $this->c = $c;
+    }
+
+    /**
+     * Specify data which should be serialized to JSON
+     * @link https://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    public function jsonSerialize()
+    {
+        return [
+            'a' => $this->a,
+            'b' => $this->b,
+            'c' => $this->c,
+        ];
+    }
+}
+
+print_r(json_encode(new Test(1,2,2)));
+
