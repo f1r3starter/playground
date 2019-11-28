68fce5367d51e59f02441ac8f9e747fe08a845c0
f1r3starter <andrey.filenochko@gmail.com>
2019-11-28 22:02:17 +0200

    fixes

--- algorithms/bubble_sort.php
+++ algorithms/bubble_sort.php
@@ -2,7 +2,7 @@
 
 function bubbleSort(array $arr): array
 {
-    for ($i = 0; $i < count($arr); $i++) {
+    for ($i = 0, $iMax = count($arr); $i < $iMax; $i++) {
         for ($j = 0; $j < count($arr) - 1; $j++) {
 
             if ($arr[$j] > $arr[$j + 1]) {
