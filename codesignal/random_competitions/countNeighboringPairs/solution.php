9678d2c9e5fff8754369eccb0af2ed2c345bc01e
f1r3starter <andrey.filenochko@gmail.com>
2019-12-05 21:57:22 +0200

    cleanup

--- codesignal/random_competitions/countNeighboringPairs/solution.php
+++ codesignal/random_competitions/countNeighboringPairs/solution.php
@@ -3,7 +3,7 @@
 function countNeighbouringPairs($a)
 {
     $a = str_split($a);
-    for ($i = 1; $i < count($a); $i++) {
+    for ($i = 1, $iMax = count($a); $i < $iMax; $i++) {
         $a[$i] != $a[$i - 1] ?: ++$t;
     }
     return $t ?: 0;
