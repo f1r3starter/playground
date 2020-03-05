a66bbcade1afa60357badadc464b86b4034c64bf
f1r3starter <andrey.filenochko@gmail.com>
2020-03-05 23:58:17 +0200

    rr

--- eaa/transaction_script/src/DB/MyPDO.php
+++ eaa/transaction_script/src/DB/MyPDO.php
@@ -3,17 +3,19 @@
 namespace App\DB;
 
 use PDO;
+use PDOStatement;
 
 class MyPDO extends PDO
 {
     /**
      * MyPDO constructor.
+     *
      * @param string|null $dsn
      * @param string|null $username
      * @param string|null $password
      * @param array|null $options
      */
-    public function __construct(?string $dsn, ?string $username, ?string $password , ?array $options)
+    public function __construct(?string $dsn, ?string $username, ?string $password, ?array $options)
     {
         $defaultOptions = [
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -28,17 +30,15 @@ class MyPDO extends PDO
      * @param string $sql
      * @param array|null $args
      *
-     * @return bool|false|\PDOStatement
+     * @return bool|false|PDOStatement
      */
     public function run(string $sql, ?array $args)
     {
-        if ($args)
-        {
+        if ($args) {
             $stmt = $this->prepare($sql);
             $stmt->execute($args);
 
             return $stmt;
-
         }
 
         return $this->query($sql);
